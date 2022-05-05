16de9331ce25137fe51625e3e5a0ac26459301ae
MitikaT <77575618+MitikaT@users.noreply.github.com>
2022-05-05 22:40:49 +0530

    Create 01FindTheDuplicateNumber.cpp

---
+++ 01FindTheDuplicateNumber.cpp
@@ -0,0 +1,26 @@
+class Solution{
+public:
+    int findDuplicate(vector<int>& nums){
+        
+        int num;
+        
+        for(int i=0;i<nums.size();i++){
+            
+            int n=nums[abs(nums[i])];
+            
+            if(n<0){
+                num=nums[i];
+                break;
+            }
+            
+            nums[abs(nums[i])]=-nums[abs(nums[i])];
+        
+        }
+        
+        for(int i=0;i<nums.size();i++)
+            cout<<nums[i];
+        
+        return abs(num);
+    
+    }
+};
